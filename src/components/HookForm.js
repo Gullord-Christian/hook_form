67498465d373b3e50d5709fd86e94fe238551b94
Christian Gullord <gullord.christian@gmail.com>
2022-07-08 00:33:07 -0500

    MoreForms

--- src/components/HookForm.js
+++ src/components/HookForm.js
@@ -12,22 +12,27 @@ const HookForm = (props) => {
             <div>
                 <label>First Name: </label>
                 <input type="text" onChange={(e) => setFirstName(e.target.value)} />
+                {!firstName ? <span> Please enter a first name </span> : null }
             </div>
             <div>
                 <label>Last Name: </label>
                 <input type="text" onChange={(e) => setLastName(e.target.value)} />
+                {!lastName ? <span> Please enter a last name </span> : null}
             </div>
             <div>
                 <label>Email: </label>
                 <input type="text" onChange={(e) => setEmail(e.target.value)} />
+                {!email ? <span> Please enter an email</span> : null}
             </div>
             <div>
                 <label>Password: </label>
                 <input type="text" onChange={(e) => setPassword(e.target.value)} />
+                {!password ? <span> Please enter a password </span> : null}
             </div>
             <div>
                 <label>Confirm Password: </label>
                 <input type="text" onChange={(e) => setConfirmPassword(e.target.value)} />
+                {!confirmPassword ? <span> Please enter password confirmation </span> : null}
             </div>
             <div>
                 <p> First Name - {firstName} </p>
